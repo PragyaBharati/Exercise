5b265b34320b0609cf62c65eae64fe6d3260bd1d
PragyaBharati <pragyabharati31@gmail.com>
2021-03-15 16:36:24 +0530

    Second Commit

---
+++ WebAppExGit/Data/WebAppExGitContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppExGit.Data
+{
+    public class WebAppExGitContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebAppExGitContext() : base("name=WebAppExGitContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebAppExGit.Models.Emp> Emps { get; set; }
+    }
+}
--- WebAppExGit/Models/Emp.cs
+++ WebAppExGit/Models/Emp.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WebAppExGit.Models
 {
+    [Table("Emp")]
     public class Emp
     {
+        [Key]
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string EDesing { get; set; }
+        public DateTime EDOJ { get; set; }
     }
 }
